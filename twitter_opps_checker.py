a5da3e2c6d206c8d5d3321a883837b754790e094
Z <z@0x0z.me>
2023-10-19 11:16:56 -0500

    Update twitter_opps_checker.py

--- twitter_opps_checker.py
+++ twitter_opps_checker.py
@@ -1,34 +1,42 @@
 import tweepy
 
-# Set up the tweepy authorization
-access_token = "YOUR_ACCESS_TOKEN"
-access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
-bearer_token = "YOUR_BEARER_TOKEN"
+# Your credentials
+API_KEY = ""
+API_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
 
-auth = tweepy.OAuthHandler(access_token, access_token_secret, bearer_token)
+# Set up Tweepy with OAuth1a
+auth = tweepy.OAuth1UserHandler(
+    consumer_key=API_KEY,
+    consumer_secret=API_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET
+)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
-api = tweepy.API(auth)
+def get_user_id(screen_name):
+    """Retrieve the user ID for a screen name using Tweepy."""
+    user = api.get_user(screen_name=screen_name)
+    return user.id_str
 
-# Function to get all followers of a user
-def get_all_followers(screen_name):
+def get_followers(user_id):
+    """Retrieve followers for a user ID using Tweepy."""
     followers = []
-    for page in tweepy.Cursor(api.followers_ids, screen_name=screen_name).pages():
-        followers.extend(page)
+    for follower in tweepy.Cursor(api.get_follower_ids, user_id=user_id).items():
+        followers.append(follower)
     return followers
 
-# Your screen name and the screen name of the user you've blocked
-your_screen_name = "YOUR_SCREEN_NAME"
-blocked_screen_name = "BLOCKED_USER_SCREEN_NAME"
-
-your_followers = get_all_followers(your_screen_name)
-blocked_user_followers = get_all_followers(blocked_screen_name)
-
-# Check for overlaps
-overlap_followers = set(your_followers).intersection(blocked_user_followers)
-
-# Convert user IDs back to screen names
-overlap_followers_screen_names = [api.get_user(user_id).screen_name for user_id in overlap_followers]
-
-# Output the list
-for screen_name in overlap_followers_screen_names:
-    print("@" + screen_name)
+if __name__ == "__main__":
+    YOUR_SCREEN_NAME = "you"
+    BLOCKED_SCREEN_NAME = "opps"
+    
+    # Convert screen names to user IDs
+    YOUR_USER_ID = get_user_id(YOUR_SCREEN_NAME)
+    BLOCKED_USER_ID = get_user_id(BLOCKED_SCREEN_NAME)
+    
+    your_followers = get_followers(YOUR_USER_ID)
+    blocked_followers = get_followers(BLOCKED_USER_ID)
+    
+    overlapping_followers = set(your_followers).intersection(set(blocked_followers))
+    print("Overlapping follower IDs:", overlapping_followers)
